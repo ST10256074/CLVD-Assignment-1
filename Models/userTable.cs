ca3d81b23b36d15c8f318d64f8fa251f7cadcf7e
James Hart <st10256074@vcconnect.edu.za>
2024-03-27 11:16:49 +0200

    Add project files.

---
+++ Models/userTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+
+namespace Cloud_Aissgnment_1.Models
+{
+    public class userTable : Controller
+    {
+
+        public static string con_string = "Server=tcp:st10256074-sql-server.database.windows.net,1433;Initial Catalog=st10256074-sql-db;Persist Security Info=False;User ID=James;Password=qTSJh2lbCrIRs5cSDvW6jhFkyUtyTX64;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection(con_string);
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
